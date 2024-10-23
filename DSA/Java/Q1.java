917a6035445db476694de01f9773dbe35b52a803
shreya-22-s <shreyasingh802302@gmail.com>
2024-10-22 22:07:56 -0700

    Update Q1.java

--- DSA/Java/Q1.java
+++ DSA/Java/Q1.java
@@ -1,6 +1,16 @@
 public class Q1 {
     public static void generatePascalTriangle(int n) {
-        //write your code here
+         for(int i=1;i<=n;i++){
+             for(int j=0;j<=n-i;j++){
+                 System.out.println(" ");
+             }
+             int c=1;
+             for(int j=1;j<=i;j++){
+                 System.out.println(c+ " ");
+                 c=c*(i-j)/j;
+             }
+             System.out.println();
+         }
     }
 
     public static void main(String[] args) {
