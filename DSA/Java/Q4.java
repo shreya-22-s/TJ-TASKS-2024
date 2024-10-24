dfeb36426aa284cf1f0b8ee432b2c378d44c1fef
shreya-22-s <shreyasingh802302@gmail.com>
2024-10-24 00:08:33 -0700

    Update Q4.java

--- DSA/Java/Q4.java
+++ DSA/Java/Q4.java
@@ -1,6 +1,33 @@
 public class Q4 {
     public static int compress(char[] chars) {
         //write your code here
+                int index = 0; // Index to place the compressed character
+        int i = 0; // Index to iterate through the chars array
+
+        while (i < chars.length) {
+            char currentChar = chars[i];
+            int count = 0;
+
+            // Count occurrences of the current character
+            while (i < chars.length && chars[i] == currentChar) {
+                i++;
+                count++;
+            }
+
+            // Place the character
+            chars[index++] = currentChar;
+
+            // If the count is greater than 1, place the digits of the count
+            if (count > 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        // Return the new length of the compressed array
+        return index;
+
     }
 
     public static void main(String[] args) {
